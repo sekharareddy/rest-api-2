5c334b33393cd78f20a30e9fcc4cf1e92c34f124
sekharareddy <sekharareddy@live.com>
2024-07-18 21:45:02 -0700

    added swagger documentatiion of /health endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,12 @@ const specs = require("../utils/swagger");
 const { returnStateHandler } = require("../utils/returnStateHandler");
 const { auth } = require("../middleware/auth");
 
-router.get('/api', function (req, res) { res.send("ResFrac API"); });
+// Public routes (anonymous)
+router.get('/api', function (req, res) { res.status(200).send("ResFrac API"); });
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-router.get("/health", (req, res) => res.sendStatus(200));
-router.use("/", express.static(path.join(__dirname, "./public"))); // added static html files in public folder to verify google registration
-router.get('/', function (req, res) { res.send("OK"); }, returnStateHandler);
+router.get("/health", (req, res) => res.status(200).send("ResFrac API - OK"));
+router.use("/", express.static(path.join(__dirname, "../public"))); // added static html files in public folder to verify google registration
+router.get('/', function (req, res) { res.status(200).send("OK"); });
 
 // Secure Routes
 // app.use("/organization", auth, routes.RoleRoute);
@@ -20,3 +21,19 @@ router.get('/', function (req, res) { res.send("OK"); }, returnStateHandler);
 
 module.exports = router;
 
+
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Retrieve health status of API.
+ *     tags:
+ *       - Health
+ *     responses:
+ *       '200':
+ *         description: Health status.
+ *         content:
+ *           application/text:
+ *       '500':
+ *         description: Internal server error.
+ */
